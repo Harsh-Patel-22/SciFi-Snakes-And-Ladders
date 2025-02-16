c33e98d5f472304dfb99397a769ca9a24ebcc3a4
Harsh Patel <144122731+Harsh-Patel-22@users.noreply.github.com>
2025-02-16 11:28:48 +0530

    Added Particle Effects
    Home Screen button clicks & usual dynamic emission.

--- Assets/Scripts/Managers/HomeUIManager.cs
+++ Assets/Scripts/Managers/HomeUIManager.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 namespace SnakesAndLadders {
@@ -6,11 +8,18 @@ namespace SnakesAndLadders {
     {
         private const string PLAY_DRONE_ANIMATION = "PlayDroneAnimation";
         [SerializeField] private Animator droneAnimator;
+        [SerializeField] private ParticleSystem[] clickParticleSystem;
 
         private float droneAnimationMax = 7f;
         private float droneAnimationDelayTimer;
         public void VsButtonClicked(int index) {
-            GameManager.Instance.VsButtonCLicked(index);
+            clickParticleSystem[index].Play();
+            StartCoroutine(ExecuteAfterDelay(() => { GameManager.Instance.VsButtonCLicked(index); }, clickParticleSystem[index].main.duration + clickParticleSystem[index].main.startLifetimeMultiplier));
+        }
+
+        private IEnumerator ExecuteAfterDelay(Action callback, float delay) {
+            yield return new WaitForSeconds(delay);
+            callback();
         }
 
         private void Start() {
